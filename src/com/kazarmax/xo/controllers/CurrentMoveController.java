6639c45cb630518bf7bfabd15eaf4c45ad36da9f
Maxim Kazartsev <kazarmax@gmail.com>
2016-03-31 21:13:26 +0300

    CurrentMoveController - new class added

---
+++ src/com/kazarmax/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,40 @@
+package com.kazarmax.xo.controllers;
+
+import com.kazarmax.xo.model.Field;
+import com.kazarmax.xo.model.Figure;
+import com.kazarmax.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+
+        int countX = 0;
+        int countO = 0;
+
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < field.getSize(); j++) {
+
+                try {
+                    if (field.getFigure(new Point(i,j)) == Figure.X ) {
+                        countX++;
+                        continue;
+                    }
+
+                    if (field.getFigure(new Point(i,j)) == Figure.O ) {
+                        countO++;
+                    }
+                } catch (InvalidPointException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return countX > countO ? Figure.O : Figure.X;
+
+    }
+
+
+
+}
